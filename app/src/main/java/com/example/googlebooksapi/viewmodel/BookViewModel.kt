92ff13f338c01058d26d4d1603b6b5cd3eebd21d
Antonino AN <andonino@gmail.com>
2022-08-29 09:38:24 -0400

    Implemented network book

--- app/src/main/java/com/example/googlebooksapi/viewmodel/BookViewModel.kt
+++ app/src/main/java/com/example/googlebooksapi/viewmodel/BookViewModel.kt
@@ -1,11 +1,34 @@
 package com.example.googlebooksapi.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.googlebooksapi.model.Empty
+import com.example.googlebooksapi.model.Repository
+import com.example.googlebooksapi.model.UIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class BookViewModel : ViewModel() {
+@HiltViewModel
+class BookViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
 
-    fun searchBook(bookQuery: String, bookQuerySize: String, selectedPrintType: String) {
-        TODO("Not yet implemented")
+    private val _uiState: MutableStateFlow<UIState> = MutableStateFlow( Empty )
+    val uiState: StateFlow<UIState> get() = _uiState
+
+    fun searchBook(bookQuery: String,
+                   bookQuerySize: String,
+                   selectedPrintType: String) {
+        viewModelScope.launch {
+            repository.getBookByName(bookQuery,
+                bookQuerySize,
+                selectedPrintType).collect{
+                    _uiState.value = it
+            }
+        }
     }
 
 }
